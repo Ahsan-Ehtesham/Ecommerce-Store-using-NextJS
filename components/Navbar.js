7f427da30c1e3e576d5e662ad5b2a6ad19f12399
Ahsan-Ehtesham <ahsanehtesham110@gmail.com>
2022-04-08 18:01:35 +0500

    Updated readme.md

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => {
     return <div>
         <header className="text-gray-600 body-font shadow-lg shadow-gray-600/50">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <Link href={'/'}><a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <Image src="/logo.png" width={48} height={48} alt="Logo" />
                     <span className="ml-3 text-xl">TechWear</span>
-                </a>
+                </a></Link>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center space-x-5">
                     <Link href={'/tshirts'} className="mr-5 hover:text-gray-900">T-Shirts</Link>
                     <Link href={'/hoodies'} className="mr-5 hover:text-gray-900">Hoodies</Link>
