a20add892af775e1f607c288ed2eefd6e8f535d7
Harsh Kumar <9u.harsh@gmail.com>
2022-10-09 19:35:20 +0530

    added api confriguation

--- pages/hoodies.js
+++ pages/hoodies.js
@@ -1,98 +1,53 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import mongoose from "mongoose";
+import Product from "../models/Product";
 
 const Hoodies = () => {
-    return (
-        <div>
-            <section className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-wrap -m-4">
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <Link href={"/products/hoodies"}><a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-scale-down object-top w-full h-full block" src="https://m.media-amazon.com/images/I/91I9jbQY2XL._AC_UX569_.jpg" />
-                            </a>
-                            </Link>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HOODIES</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Russell Athletic Men's Dri-Power Pullover Fleece Hoodie</h2>
-                                <p className="mt-1">₨2,200</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-scale-down object-top w-full h-full block" src="https://m.media-amazon.com/images/I/91I9jbQY2XL._AC_UX569_.jpg" />
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HOODIES</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Russell Athletic Men's Dri-Power Pullover Fleece Hoodie</h2>
-                                <p className="mt-1">₨2,200</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-scale-down object-top w-full h-full block" src="https://m.media-amazon.com/images/I/91I9jbQY2XL._AC_UX569_.jpg" />
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HOODIES</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Russell Athletic Men's Dri-Power Pullover Fleece Hoodie</h2>
-                                <p className="mt-1">₨2,200</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-scale-down object-top w-full h-full block" src="https://m.media-amazon.com/images/I/91I9jbQY2XL._AC_UX569_.jpg" />
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HOODIES</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Russell Athletic Men's Dri-Power Pullover Fleece Hoodie</h2>
-                                <p className="mt-1">₨2,200</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-scale-down object-top w-full h-full block" src="https://m.media-amazon.com/images/I/91I9jbQY2XL._AC_UX569_.jpg" />
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HOODIES</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Russell Athletic Men's Dri-Power Pullover Fleece Hoodie</h2>
-                                <p className="mt-1">₨2,200</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-scale-down object-top w-full h-full block" src="https://m.media-amazon.com/images/I/91I9jbQY2XL._AC_UX569_.jpg" />
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HOODIES</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Russell Athletic Men's Dri-Power Pullover Fleece Hoodie</h2>
-                                <p className="mt-1">₨2,200</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-scale-down object-top w-full h-full block" src="https://m.media-amazon.com/images/I/91I9jbQY2XL._AC_UX569_.jpg" />
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HOODIES</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Russell Athletic Men's Dri-Power Pullover Fleece Hoodie</h2>
-                                <p className="mt-1">₨2,200</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-scale-down object-top w-full h-full block" src="https://m.media-amazon.com/images/I/91I9jbQY2XL._AC_UX569_.jpg" />
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HOODIES</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Russell Athletic Men's Dri-Power Pullover Fleece Hoodie</h2>
-                                <p className="mt-1">₨2,200</p>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            {products.map((item) => {
+              return (
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={item.id}>
+                  <Link href={"/products/hoodies"}>
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      <img
+                        alt="ecommerce"
+                        className="object-scale-down object-top w-full h-full block"
+                        src={item.img}
+                      />
+                    </a>
+                  </Link>
+                  <div className="mt-4">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                      {item.category}
+                    </h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">
+                      {item.title}
+                    </h2>
+                    <p className="mt-1">₨{item.price}</p>
+                  </div>
                 </div>
-            </section>
+              );
+            })}
+          </div>
         </div>
-    )
+      </section>
+    </div>
+  );
+};
+
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+  let products = await Product.find();
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) }, // will be passed to the page component as props
+  };
 }
 
-export default Hoodies
+export default Hoodies;
