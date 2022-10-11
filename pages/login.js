43e09f0edd38fc1f40ecd585fba5833b970a1ef0
jonathans199c <jonathasa@city-furniture.com>
2022-10-10 21:14:28 -0400

    added signup and login logic to capture users input and data is ready to be sent to an API endpoint

--- pages/login.js
+++ pages/login.js
@@ -1,99 +1,110 @@
-import React from "react";
-import Link from "next/link";
+import React, { useState } from 'react'
+import Link from 'next/link'
 
 const Login = () => {
+  const [form, setForm] = useState({})
+
+  const handleFormFields = e => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleFormSubmit = e => {
+    // todo: need to update to API endpoint to handle POST request
+    fetch('/api/***API TO HANDLE*** login', {
+      method: POST,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err))
+  }
+
   return (
     <>
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
+      <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+        <div className='max-w-md w-full space-y-8'>
           <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="/logo.png"
-              alt="Workflow"
-            />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Log in to your account
-            </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
+            <img className='mx-auto h-12 w-auto' src='/logo.png' alt='Workflow' />
+            <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>Log in to your account</h2>
+            <p className='mt-2 text-center text-sm text-gray-600'>
               Or&nbsp;
-              <span className="font-medium text-blue-600 hover:text-blue-500">
-                <Link href={"/signup"}>sign up for a new account</Link>
+              <span className='font-medium text-blue-600 hover:text-blue-500'>
+                <Link href={'/signup'}>sign up for a new account</Link>
               </span>
             </p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" value="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
+          <form className='mt-8 space-y-6' action='submit' onSubmit={handleFormSubmit} method='POST'>
+            <input type='hidden' name='remember' value='true' onChange={handleFormFields} />
+            <div className='rounded-md shadow-sm -space-y-px'>
               <div>
-                <label htmlFor="email-address" className="sr-only">
+                <label htmlFor='email-address' className='sr-only'>
                   Email address
                 </label>
                 <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id='email-address'
+                  name='email'
+                  type='email'
+                  autoComplete='email'
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
+                  className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm'
+                  placeholder='Email address'
+                  onChange={handleFormFields}
                 />
               </div>
               <div>
-                <label htmlFor="password" className="sr-only">
+                <label htmlFor='password' className='sr-only'>
                   Password
                 </label>
                 <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
+                  id='password'
+                  name='password'
+                  type='password'
+                  autoComplete='current-password'
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
+                  className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm'
+                  placeholder='Password'
+                  onChange={handleFormFields}
                 />
               </div>
             </div>
 
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center'>
                 <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                  id='remember-me'
+                  name='remember-me'
+                  type='checkbox'
+                  className='h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded'
                 />
-                <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  {" "}
-                  Remember me{" "}
+                <label htmlFor='remember-me' className='ml-2 block text-sm text-gray-900'>
+                  {' '}
+                  Remember me{' '}
                 </label>
               </div>
 
-              <div className="text-sm font-medium text-blue-600 hover:text-blue-500">
-                <Link href={"/forgotpassword"}>Forgot your password?</Link>
+              <div className='text-sm font-medium text-blue-600 hover:text-blue-500'>
+                <Link href={'/forgotpassword'}>Forgot your password?</Link>
               </div>
             </div>
 
             <div>
               <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                type='submit'
+                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'>
+                <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
                   <svg
-                    className="h-5 w-5 text-blue-500 group-hover:text-blue-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
+                    className='h-5 w-5 text-blue-500 group-hover:text-blue-400'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 20 20'
+                    fill='currentColor'
+                    aria-hidden='true'>
                     <path
-                      fillRule="evenodd"
-                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                      clipRule="evenodd"
+                      fillRule='evenodd'
+                      d='M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z'
+                      clipRule='evenodd'
                     />
                   </svg>
                 </span>
@@ -104,7 +115,7 @@ const Login = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
